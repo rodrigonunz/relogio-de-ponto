ee79645c47cce94eee7d9845320716de3e9e5567
Rodrigo Nunes <rodrigonunes@live.com>
2018-02-03 16:27:06 -0200

    Primeiro commit

---
+++ src/main/java/br/com/deponto/relogiodeponto/RelogioDePontoApplication.java
@@ -0,0 +1,12 @@
+package br.com.deponto.relogiodeponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelogioDePontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelogioDePontoApplication.class, args);
+	}
+}
